89143ad455a99af4e8b9faee2ab119afa6a2d70e
Wasosky <110636486+0xWasosky@users.noreply.github.com>
2024-03-03 15:42:04 +0100

    Add files via upload

---
+++ SecurePassPhraseGen/main.py
@@ -0,0 +1,32 @@
+import json
+import time
+import secrets
+import hashlib
+
+from typing import Iterable
+
+t = time.time()
+
+with open("worlds.json", 'r') as f:
+    worlds = json.loads(f.read())['worlds']
+
+te1 = time.time()
+print(f"Worlds loaded in {te1 - t}")
+
+def algorithm(worlds: Iterable = worlds) -> tuple:
+    rand_number = 0
+    phrase = ""
+
+    n = secrets.choice(range(10, 30))
+
+    while n > 1:
+        rand_number = secrets.randbelow(11)
+
+        if rand_number % 2 == 0:
+            phrase += secrets.choice(worlds).title() + ' '
+        else:
+            phrase += secrets.choice(worlds) + ' '  
+
+        n -= 1
+    
+    return phrase, hashlib.sha512(phrase.encode()).hexdigest()
